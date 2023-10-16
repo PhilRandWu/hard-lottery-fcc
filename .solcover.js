c370c8a340ad3e6bf6fb2e6394ae8a455361e257
Wenbo.Wu <3378814052@qq.com>
2023-10-16 16:31:16 +0800

    config: plugin config

---
+++ .solcover.js
@@ -0,0 +1,8 @@
+// module.exports = {
+//     skipFiles: [
+//         'interfaces/', // 忽略 interfaces 目录下的所有文件
+//         'mocks/', // 忽略 mocks 目录下的所有文件
+//         'test/', // 忽略 test 目录下的所有文件
+//     ],
+//     istanbulReporter: ['html', 'text'],
+// };
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,21 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import {HardhatUserConfig} from "hardhat/config";
+import '@nomicfoundation/hardhat-toolbox';
+import 'dotenv';
+import '@nomicfoundation/hardhat-verify';
+import 'hardhat-gas-reporter';
+import 'solidity-coverage';
+import 'hardhat-deploy';
+import '@nomicfoundation/hardhat-ethers';
+import 'hardhat-deploy-ethers';
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+    solidity: "0.8.19",
+    defaultNetwork: 'hardhat',
+    networks: {
+        hardhat: {
+            chainId: 31337
+        }
+    }
 };
 
 export default config;
