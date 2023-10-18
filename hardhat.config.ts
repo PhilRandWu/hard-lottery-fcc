aa2afa6c5c690ae49d49d49df0a883067975f91f
Wenbo.Wu <3378814052@qq.com>
2023-10-18 17:31:52 +0800

    config: hardhat utils

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,4 +1,4 @@
-import {HardhatUserConfig} from "hardhat/config";
+import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
 import 'dotenv';
 import '@nomicfoundation/hardhat-verify';
@@ -8,12 +8,34 @@ import 'hardhat-deploy';
 import '@nomicfoundation/hardhat-ethers';
 import 'hardhat-deploy-ethers';
 
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
+
 const config: HardhatUserConfig = {
-    solidity: "0.8.19",
+    solidity: {
+        compilers: [
+            {
+                version: '0.8.9',
+            },
+        ],
+    },
     defaultNetwork: 'hardhat',
     networks: {
         hardhat: {
-            chainId: 31337
+            chainId: 31337,
+        },
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0, // here this will by default take the first account as deployer
+            1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+        },
+        player: {
+            default: 1,
+        },
+    },
+    etherscan: {
+        apiKey: {
+            sepolia: ETHERSCAN_API_KEY,
         }
     }
 };
